6ff5c9caac85ffb136983af47a089788c23239ff
albertrocha031 <albertrocha031@gmail.com>
2024-02-20 18:02:29 -0500

    homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <h1>Blog</h1>
+      <Homepage />
     </div>
   );
 }
